750c1dc344e22c4a18bef08c19934ed249d33885
YUGON EOM <lauricacid@naver.com>
2020-04-02 00:24:21 +0900

    20200402

--- VanilaJS/todo.js
+++ VanilaJS/todo.js
@@ -4,7 +4,18 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
+
+function deleteTodo(event){
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo){
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
 
 function saveToDos(){
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -16,6 +27,7 @@ function paintToDo(text){
   const span = document.createElement("span");
   const newId = toDos.length + 1;
   delBtn.innerHTML = "❌";
+  delBtn.addEventListener("click", deleteTodo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
@@ -39,6 +51,10 @@ function handleSubmit(event){
 function loadToDos(){
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if(loadedToDos !== null){
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo){
+      paintToDo(toDo.text);
+    });
   }
 }
 
