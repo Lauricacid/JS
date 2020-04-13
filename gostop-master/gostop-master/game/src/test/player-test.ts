80cfc1f2b8f52e8bb3e03fa1233c3036b2ea7a1a
YUGON EOM <lauricacid@naver.com>
2020-04-13 23:06:27 +0900

    20200413

--- gostop-master/gostop-master/game/src/test/player-test.ts
+++
@@ -1,9 +0,0 @@
-import {Player} from '../main/player';
-import {expect} from 'chai';
-import 'mocha';
-
-describe('player test', ()=>{
-  it('initialize ok', ()=>{
-    let player = new Player('Player1', 10000000, null);
-  })
-})
